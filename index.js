33d363ae666cc43f4b06dce9e877371eea3288d4
David T. <davidt.info@gmail.com>
2025-01-18 09:45:38 -0300

    Criar elementos

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ const inputItem = document.getElementById("input-item");
 const botaoAdicionar = document.getElementById("adicionar-item");
 botaoAdicionar.addEventListener("click", (evento) => {
     evento.preventDefault();
-    console.log(inputItem.value)
+    if (inputItem.value === "") {
+        alert("Por favor, insira um item!");
+        return;
+    }
+
+    const itemDaLista = document.createElement("li");
+    console.log(itemDaLista);
 });
