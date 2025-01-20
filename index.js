9c2e20ac7e1fd2df851c5ca3cd343a50de84f8ad
David T. <davidt.info@gmail.com>
2025-01-20 09:07:14 -0300

    Lista vazia

--- index.js
+++ index.js
@@ -46,8 +46,21 @@ botaoAdicionar.addEventListener("click", (evento) => {
     itemDaLista.appendChild(itemData);
     listaDeCompras.appendChild(itemDaLista);
 
-    
+    verficarListaVazia();
 
 
 
 });
+
+const mensagemListaVazia = document.querySelector('.mensagem-lista-vazia');
+
+function verficarListaVazia(){
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+    if (itensDaLista.length === 0){
+        mensagemListaVazia.style.display = "block";
+    } else {
+        mensagemListaVazia.style.display = "none";
+    }
+}
+
+verficarListaVazia();
